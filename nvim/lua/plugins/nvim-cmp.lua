10598c0b8c2b79be6a0ab907b7985a57a3e408cd
wforeros <wilson.forero@trespimedios.co>
2024-08-06 12:25:04 -0500

    Add pretty format for nvim-cmp

---
+++ nvim/lua/plugins/nvim-cmp.lua
@@ -0,0 +1,35 @@
+return {
+  {
+    "hrsh7th/nvim-cmp",
+    opts = function(_, opts)
+      opts.window = {
+        completion = {
+          border = {
+            { "󱐋", "WarningMsg" },
+            { "─", "Comment" },
+            { "╮", "Comment" },
+            { "│", "Comment" },
+            { "╯", "Comment" },
+            { "─", "Comment" },
+            { "╰", "Comment" },
+            { "│", "Comment" },
+          },
+          winblend = 0,
+        },
+        documentation = {
+          border = {
+            { "󰙎", "DiagnosticHint" },
+            { "─", "Comment" },
+            { "╮", "Comment" },
+            { "│", "Comment" },
+            { "╯", "Comment" },
+            { "─", "Comment" },
+            { "╰", "Comment" },
+            { "│", "Comment" },
+          },
+          winblend = 0,
+        },
+      }
+    end,
+  },
+}
