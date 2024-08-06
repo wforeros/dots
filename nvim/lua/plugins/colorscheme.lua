048794812325116f8ac67029d32325d12c4c14e3
wforeros <wilson.forero@trespimedios.co>
2024-08-06 12:23:33 -0500

    Add transparent background

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -5,49 +5,88 @@ return {
     priority = 1000,
     opts = {},
   },
-  {
-    "gmr458/vscode_modern_theme.nvim",
-    lazy = false,
-    priority = 1000,
-    config = function()
-      require("vscode_modern").setup({
-        cursorline = true,
-        transparent_background = false,
-        nvim_tree_darker = true,
-      })
-      vim.cmd.colorscheme("vscode_modern")
-    end,
-  },
   -- {
-  --   "olimorris/onedarkpro.nvim",
-  --   priority = 1000, -- Ensure it loads first
+  --   "gmr458/vscode_modern_theme.nvim",
+  --   lazy = false,
+  --   priority = 1000,
+  --   config = function()
+  --     require("vscode_modern").setup({
+  --       cursorline = true,
+  --       transparent_background = true,
+  --       nvim_tree_darker = true,
+  --     })
+  --     vim.cmd.colorscheme("vscode_modern")
+  --   end,
   -- },
-  -- config = function()
-  --   require("onedarkpro").setup({
-  --     colors = {}, -- Override default colors or create your own
-  --     highlights = {}, -- Override default highlight groups or create your own
-  --     styles = { -- For example, to apply bold and italic, use "bold,italic"
-  --       types = "BOLD", -- Style that is applied to types
-  --       methods = "BOLD", -- Style that is applied to methods
-  --       numbers = "NONE", -- Style that is applied to numbers
-  --       strings = "NONE", -- Style that is applied to strings
-  --       comments = "NONE", -- Style that is applied to comments
-  --       keywords = "NONE", -- Style that is applied to keywords
-  --       constants = "NONE", -- Style that is applied to constants
-  --       functions = "BOLD", -- Style that is applied to functions
-  --       operators = "NONE", -- Style that is applied to operators
-  --       variables = "NONE", -- Style that is applied to variables
-  --       parameters = "NONE", -- Style that is applied to parameters
-  --       conditionals = "NONE", -- Style that is applied to conditionals
-  --       virtual_text = "NONE", -- Style that is applied to virtual text
-  --     },
-  --   })
-  -- end,
+  { "catppuccin/nvim", name = "catppuccin", priority = 1001, lazy = false, opts = { flavour = "macchiato" } },
+  {
+    "xiyaowong/transparent.nvim",
+    config = function()
+      require("transparent").setup({ -- Optional, you don't have to run setup.
+        groups = { -- table: default groups
+          "Normal",
+          "NormalNC",
+          "Comment",
+          "Constant",
+          "Special",
+          "Identifier",
+          "Statement",
+          "PreProc",
+          "Type",
+          "Underlined",
+          "Todo",
+          "String",
+          "Function",
+          "Conditional",
+          "Repeat",
+          "Operator",
+          "Structure",
+          "LineNr",
+          "NonText",
+          "SignColumn",
+          "CursorLine",
+          "CursorLineNr",
+          "StatusLine",
+          "StatusLineNC",
+          "EndOfBuffer",
+        },
+        extra_groups = {
+          "NormalFloat", -- plugins which have float panel such as Lazy, Mason, LspInfo
+          "NvimTreeNormal", -- NvimTree
+          "NvimTreeNormalNC", -- NvimTree
+        },
+        exclude_groups = {}, -- table: groups you don't want to clear
+      })
+    end,
+    {
+      {
+        "scottmckendry/cyberdream.nvim",
+        lazy = false,
+        priority = 1000,
+        opts = function(_, opts)
+          opts.transparent = true
+          opts.italic_comments = true
+        end,
+      },
+      {
+        "LazyVim/LazyVim",
+        opts = {
+          colorscheme = "cyberdream",
+        },
+      },
+
+      -- modicator (auto color line number based on vim mode)
+      {
+        "mawkler/modicator.nvim",
+        dependencies = "scottmckendry/cyberdream.nvim",
+        init = function()
+          -- These are required for Modicator to work
+          vim.o.cursorline = false
+          vim.o.number = true
+          vim.o.termguicolors = true
+        end,
+        opts = {},
+      },
+    },
+  },
 }
--- { "catppuccin/nvim", name = "catppuccin", lazy = false, opts = { flavour = "macchiato" } },
--- {
---   "LazyVim/LazyVim",
---   opts = {
---     colorscheme = "catppuccin",
---   },
--- },
