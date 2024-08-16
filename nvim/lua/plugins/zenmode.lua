6c4b04a5844900f74e443de8b57e30874e2ae456
wforeros <wilson.forero@trespimedios.co>
2024-08-16 16:38:59 -0500

    feat: Add UI improvements
    Lsp boxes with a new border style
    Add multicursor functionality similar to VSCode
    Add Zen mode

---
+++ nvim/lua/plugins/zenmode.lua
@@ -0,0 +1,30 @@
+return {
+  {
+    "folke/zen-mode.nvim",
+    keys = {
+      {
+        "<leader>z",
+        function()
+          require("zen-mode").toggle()
+        end,
+        desc = "Zen Mode",
+      },
+    },
+    opts = {
+      window = {
+        width = 0.85,
+      },
+      plugins = {
+        options = {
+          laststatus = 0,
+        },
+        twilight = {
+          enabled = false,
+        },
+      },
+      on_open = function()
+        require("package-info").hide()
+      end,
+    },
+  },
+}
