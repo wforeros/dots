0e0777d963c3d631f4206afb94e60b9d492b04c3
wforeros <wilson.forero@trespimedios.co>
2024-08-20 23:33:24 -0500

    fix: colorschemas

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -2,23 +2,49 @@ return {
   {
     "rebelot/kanagawa.nvim",
     lazy = false,
-    priority = 1000,
     opts = {},
   },
-  -- {
-  --   "gmr458/vscode_modern_theme.nvim",
-  --   lazy = false,
-  --   priority = 1000,
-  --   config = function()
-  --     require("vscode_modern").setup({
-  --       cursorline = true,
-  --       transparent_background = true,
-  --       nvim_tree_darker = true,
-  --     })
-  --     vim.cmd.colorscheme("vscode_modern")
-  --   end,
-  -- },
-  { "catppuccin/nvim", name = "catppuccin", priority = 1001, lazy = false, opts = { flavour = "macchiato" } },
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    opts = {},
+  },
+  -- { "kartikp10/noctis.nvim", dependencies = { "rktjmp/lush.nvim" } },
+  {
+    "navarasu/onedark.nvim",
+    config = function()
+      require("onedark").setup({
+        style = "cool",
+      })
+    end,
+  },
+  {
+    "gmr458/vscode_modern_theme.nvim",
+    lazy = false,
+    config = function()
+      require("vscode_modern").setup({
+        cursorline = true,
+        transparent_background = true,
+        nvim_tree_darker = true,
+      })
+    end,
+  },
+  { "catppuccin/nvim", name = "catppuccin", lazy = false, opts = { flavour = "macchiato" } },
+  {
+    "scottmckendry/cyberdream.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = function(_, opts)
+      opts.transparent = true
+      opts.italic_comments = true
+    end,
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "cyberdream",
+    },
+  },
   {
     "xiyaowong/transparent.nvim",
     config = function()
@@ -58,35 +84,74 @@ return {
         exclude_groups = {}, -- table: groups you don't want to clear
       })
     end,
-    {
-      {
-        "scottmckendry/cyberdream.nvim",
-        lazy = false,
-        priority = 1000,
-        opts = function(_, opts)
-          opts.transparent = true
-          opts.italic_comments = true
-        end,
-      },
-      {
-        "LazyVim/LazyVim",
-        opts = {
-          colorscheme = "cyberdream",
-        },
-      },
-
-      -- modicator (auto color line number based on vim mode)
-      {
-        "mawkler/modicator.nvim",
-        dependencies = "scottmckendry/cyberdream.nvim",
-        init = function()
-          -- These are required for Modicator to work
-          vim.o.cursorline = false
-          vim.o.number = true
-          vim.o.termguicolors = true
-        end,
-        opts = {},
-      },
-    },
   },
+  -- modicator (auto color line number based on vim mode)
+  {
+    "mawkler/modicator.nvim",
+    dependencies = "scottmckendry/cyberdream.nvim",
+    init = function()
+      -- These are required for Modicator to work
+      vim.o.cursorline = false
+      vim.o.number = true
+      vim.o.termguicolors = true
+    end,
+    opts = {
+      show_warnings = true,
+    },
+    config = function()
+      require("modicator").setup({
+        -- Warn if any required option is missing. May emit false positives if some
+        -- other plugin modifies them, which in that case you can just ignore
+        show_warnings = false,
+        highlights = {
+          -- Default options for bold/italic
+          defaults = {
+            bold = false,
+            italic = false,
+          },
+        },
+        integration = {
+          lualine = {
+            enabled = true,
+            -- Letter of lualine section to use (if `nil`, gets detected automatically)
+            mode_section = nil,
+            -- Whether to use lualine's mode highlight's foreground or background
+            highlight = "bg",
+          },
+        },
+      })
+    end,
+  },
+  --     {
+  --       "lazyvim/lazyvim",
+  jdw, --       opts = {
+  --     --         colorscheme = "cyberdream",
+  --     --       },
+  --     --     },
+  --     --   {
+  --     {
+  --       "scottmckendry/cyberdream.nvim",
+  --       lazy = false,
+  --       priority = 1000,
+  --       opts = function(_, opts)
+  --         opts.transparent = true
+  --         opts.italic_comments = true
+  --         opts.theme = {}
+  --       end,
+  --     },
+  --     --
+  --     -- -- modicator (auto color line number based on vim mode)
+  --     -- {
+  --     --   "mawkler/modicator.nvim",
+  --     --   dependencies = "scottmckendry/cyberdream.nvim",
+  --     --   init = function()
+  --     --     -- These are required for Modicator to work
+  --     --     vim.o.cursorline = false
+  --     --     vim.o.number = true
+  --     --     vim.o.termguicolors = true
+  --     --   end,
+  --     --   opts = {},
+  --     -- },
+  --   },
+  -- },
 }
