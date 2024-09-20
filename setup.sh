ad97624b85bab7cec93337223f1ee6df8dbb23e8
wforeros <wilson.forero@trespimedios.co>
2024-09-20 00:03:50 -0500

    fix: Modify order to create symlinks after installing everything

--- setup.sh
+++ setup.sh
@@ -4,21 +4,19 @@ os=$(uname)
 echo "OS: $os"
 mkdir -p "$XDG_CONFIG_HOME"
 
-ln -s "$PWD/nvim" "$XDG_CONFIG_HOME/nvim"
-
-ln -s "$PWD/.zshrc" "$HOME/.zshrc"
-ln -s "$PWD/.tmux.conf" "$HOME/.tmux.conf"
-ln -s "$PWD/.gitconfig" "$HOME/.gitconfig"
-
 if [ "$os" = "Darwin" ]; then
 	echo "Setting up MacOS"
 	brew install zsh
 	brew install neovim
 	brew install tmux
-elif [ "$os" = "Linnux" ]; then
+elif [ "$os" = "Linux" ]; then
 	echo "Setting up Linux"
 	sudo apt install zsh
 	sudo apt install tmux
 	sudo apt install neovim
 fi
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+
+ln -s "$PWD/.zshrc" "$HOME/.zshrc"
+ln -s "$PWD/tmux/tmux.conf" "$HOME/.tmux.conf"
+ln -s "$PWD/.gitconfig" "$HOME/.gitconfig"
