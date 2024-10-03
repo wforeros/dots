8321b654a2b29e22e6fce8da41797782bf29f719
wforeros <wilson.forero@trespimedios.co>
2024-10-02 23:20:26 -0500

    feat: Set htmldjango filetype for every html file

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -1,3 +1,17 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+vim.api.nvim_create_autocmd("BufNewFile", {
+  pattern = "*.html",
+  callback = function()
+    vim.opt.filetype = "htmldjango"
+  end,
+})
+
+vim.api.nvim_create_autocmd("BufRead", {
+  pattern = "*.html",
+  callback = function()
+    vim.opt.filetype = "htmldjango"
+  end,
+})
