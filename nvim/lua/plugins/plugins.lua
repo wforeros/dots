84fddf27c411a39852fa8a631b67e8aef0260071
wforeros <wilson.forero@trespimedios.co>
2024-10-02 21:40:50 -0500

    refactor: Change 'examples.lua' to 'plugins.lua'

--- nvim/lua/plugins/plugins.lua
+++ nvim/lua/plugins/plugins.lua
@@ -170,15 +170,6 @@ return {
   },
 
   -- or you can return new options to override all the defaults
-  {
-    "nvim-lualine/lualine.nvim",
-    event = "VeryLazy",
-    opts = function()
-      return {
-        --[[add your custom lualine config here]]
-      }
-    end,
-  },
 
   {
     "safv12/andromeda.vim",
