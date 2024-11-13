d482f8dfbfad7bd57670a6fa5fe027e7dd8e4e9c
wforeros <wilson.forero@trespimedios.co>
2024-11-13 09:13:26 -0500

    fix: Remove ray-x as it was creating unnecessary floating windows

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -76,13 +76,5 @@ return {
       standardrb = {},
       pyright = {},
     },
-    {
-      "ray-x/lsp_signature.nvim",
-      event = "VeryLazy",
-      opts = {},
-      config = function(_, opts)
-        require("lsp_signature").setup(opts)
-      end,
-    },
   },
 }
