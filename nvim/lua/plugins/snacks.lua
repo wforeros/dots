241bba74cbb1ff8aa2e523d3212ecdb2701ff454
wforeros <wilson_camilo@hotmail.com>
2025-09-16 02:34:57 -0500

    update picker layout

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -60,7 +60,7 @@ return {
         -- "ivy_wider_results",
         -- "ivy_wider_preview",
         -- "ivy_taller",
-        preset = "ivy",
+        preset = "default",
         -- When reaching the bottom of the results in the picker, I don't want
         -- it to cycle and go back to the top
         cycle = false,
@@ -110,6 +110,23 @@ return {
             { win = "preview", title = "{preview}", height = 0.4, border = "top" },
           },
         },
+
+        default = {
+          layout = {
+            box = "horizontal",
+            width = 0.85,
+            height = 0.85,
+            {
+              box = "vertical",
+              border = "rounded",
+              title = "{source} {live}",
+              title_pos = "center",
+              { win = "input", height = 1, border = "bottom" },
+              { win = "list", border = "none" },
+            },
+            { win = "preview", border = "rounded", width = 0.6 },
+          },
+        },
       },
       matcher = {
         frecency = true,
@@ -133,6 +150,37 @@ return {
           filename_first = true, -- display filename before the file path
           truncate = 80,
         },
+      },
+    },
+    image = {
+      enabled = true,
+      doc = {
+        -- Personally I set this to false, I don't want to render all the
+        -- images in the file, only when I hover over them
+        -- render the image inline in the buffer
+        -- if your env doesn't support unicode placeholders, this will be disabled
+        -- takes precedence over `opts.float` on supported terminals
+        inline = vim.g.neovim_mode == "skitty" and true or false,
+        -- only_render_image_at_cursor = vim.g.neovim_mode == "skitty" and false or true,
+        -- render the image in a floating window
+        -- only used if `opts.inline` is disabled
+        float = true,
+        -- Sets the size of the image
+        -- max_width = 60,
+        -- max_width = vim.g.neovim_mode == "skitty" and 20 or 60,
+        -- max_height = vim.g.neovim_mode == "skitty" and 10 or 30,
+        max_width = vim.g.neovim_mode == "skitty" and 5 or 60,
+        max_height = vim.g.neovim_mode == "skitty" and 2.5 or 30,
+        -- max_height = 30,
+        -- Apparently, all the images that you preview in neovim are converted
+        -- to .png and they're cached, original image remains the same, but
+        -- the preview you see is a png converted version of that image
+        --
+        -- Where are the cached images stored?
+        -- This path is found in the docs
+        -- :lua print(vim.fn.stdpath("cache") .. "/snacks/image")
+        -- For me returns `~/.cache/neobean/snacks/image`
+        -- Go 1 dir above and check `sudo du -sh ./* | sort -hr | head -n 5`
       },
     },
     dashboard = {
